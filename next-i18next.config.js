a52aafa6db54fa29514962ed484a484241ab1e43
24wings <731502029@qq.com>
2023-10-17 04:16:12 +0800

    m

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -2,8 +2,8 @@ const path = require("path");
 
 module.exports = {
   i18n: {
-    defaultLocale: "en",
-    locales: ["en", "de"],
+    defaultLocale: "zh",
+    locales: ["en", "de", 'zh'],
   },
   localePath: path.resolve("./public/locales"),
 };
